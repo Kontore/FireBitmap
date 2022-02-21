f7e672f08f53ec081692ee691be3a243ed0cb47b
tigerros <glacific@gmail.com>
2022-02-21 18:30:19 +0100

    Added images

--- FireBitmap.Tests/TestData.cs
+++ FireBitmap.Tests/TestData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace Kontore.FireBitmap.Tests {
 	public class TestData {
@@ -16,13 +17,13 @@ namespace Kontore.FireBitmap.Tests {
 		/// The GitHub workspace environment variable.
 		/// If it's null, the <see cref="LOCAL_PROJECT_FOLDER"/> will be used.
 		/// </summary>
-		public static string GITHUB_WORKSPACE = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
+		public static string GITHUB_WORKSPACE { get; } = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
 
 		/// <summary>
 		/// The current project folder in the GitHub Actions environment.
 		/// If <see cref="GITHUB_WORKSPACE"/> null, the <see cref="LOCAL_PROJECT_FOLDER"/> will be used.
 		/// </summary>
-		public static string GITHUB_ACTIONS_PROJECT_FOLDER = Path.Combine(GITHUB_WORKSPACE ?? "", "FireBitmap.Tests");
+		public static string GITHUB_ACTIONS_PROJECT_FOLDER { get; } = Path.Combine(GITHUB_WORKSPACE ?? "", "FireBitmap.Tests");
 		
 		/// <summary>
 		/// The bitmap objects created from the images placed in the <c>PROJECT_FOLDER\TestBitmaps</c> folder.
@@ -30,25 +31,25 @@ namespace Kontore.FireBitmap.Tests {
 		public static List<TestCaseData> TestBitmaps { get; } = new List<TestCaseData>();
 
 		/// <summary>
+		/// The supported image extensions.
+		/// </summary>
+		private static string[] ImageExtensions { get; } = new[] { ".png", ".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".gif", ".bmp", ".tiff", ".tif" };
+
+		/// <summary>
 		/// Fills all of the <see cref="TestData"/> fields and properties with values.
 		/// </summary>
 		static TestData() {
-			if (GITHUB_WORKSPACE != null) {
-				var bitmapPaths = Directory.GetFiles(Path.Combine(GITHUB_ACTIONS_PROJECT_FOLDER, "TestBitmaps"), "*.png");
+			string testBitmapsFolder = GITHUB_WORKSPACE != null
+				? Path.Combine(GITHUB_ACTIONS_PROJECT_FOLDER, "TestBitmaps")
+				: Path.Combine(LOCAL_PROJECT_FOLDER, "TestBitmaps");
 
-				foreach (var bitmapPath in bitmapPaths) {
-					var testCase = new TestCaseData(new Bitmap(Image.FromFile(bitmapPath)));
-					testCase.SetArgDisplayNames(new FileInfo(bitmapPath).Length / 1024 + "KB");
-					TestBitmaps.Add(testCase);
-				}
-			} else {
-				var bitmapPaths = Directory.GetFiles(Path.Combine(LOCAL_PROJECT_FOLDER, "TestBitmaps"), "*.png");
+			var bitmapPaths = Directory.GetFiles(testBitmapsFolder, "*.*")
+				.Where(file => ImageExtensions.Any(ex => file.EndsWith(ex, StringComparison.OrdinalIgnoreCase)));
 
-				foreach (var bitmapPath in bitmapPaths) {
-					var testCase = new TestCaseData(new Bitmap(Image.FromFile(bitmapPath)));
-					testCase.SetArgDisplayNames(new FileInfo(bitmapPath).Length / 1024 + "KB");
-					TestBitmaps.Add(testCase);
-				}
+			foreach (var bitmapPath in bitmapPaths) {
+				var testCase = new TestCaseData(new Bitmap(Image.FromFile(bitmapPath)));
+				testCase.SetArgDisplayNames(new FileInfo(bitmapPath).Length / 1024 + "KB");
+				TestBitmaps.Add(testCase);
 			}
 		}
 	}
