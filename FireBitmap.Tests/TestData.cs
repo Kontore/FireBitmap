ab4eab504cb3a8b92dde005627b1dc802c6dba49
tigerros <glacific@gmail.com>
2022-02-21 15:27:49 +0100

    Added GITHUB_ACTIONS_PROJECT_FOLDER

--- FireBitmap.Tests/TestData.cs
+++ FireBitmap.Tests/TestData.cs
@@ -6,10 +6,15 @@ using NUnit.Framework;
 namespace Kontore.FireBitmap.Tests {
 	public class TestData {
 		/// <summary>
-		/// The current project folder.
+		/// The current project folder in a local environment.
 		/// <c>".."</c> actually refers to the <c>bin</c> folder, since that's where the executables are saved.
 		/// </summary>
 		public const string PROJECT_FOLDER = @"..\..\..";
+
+		/// <summary>
+		/// The current project folder in the GitHub Actions environment.
+		/// </summary>
+		public const string GITHUB_ACTIONS_PROJECT_FOLDER = @"$GITHUB_WORKSPACE/FireBitmap/FireBitmap.Tests";
 		
 		/// <summary>
 		/// The bitmap objects created from the images placed in the <c>PROJECT_FOLDER\TestBitmaps</c> folder.
@@ -20,7 +25,7 @@ namespace Kontore.FireBitmap.Tests {
 		/// Fills all of the <see cref="TestData"/> fields and properties with values.
 		/// </summary>
 		static TestData() {
-			var bitmapPaths = Directory.GetFiles($@"TestBitmaps", "*.png");
+			var bitmapPaths = Directory.GetFiles($@"${GITHUB_ACTIONS_PROJECT_FOLDER}\TestBitmaps", "*.png");
 
 			foreach (var bitmapPath in bitmapPaths) {
 				var testCase = new TestCaseData(new Bitmap(Image.FromFile(bitmapPath)));
